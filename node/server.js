f5525ef4b3b5b68ce830a0d05e1946afb9bd7f5c
Administrator <Administrator@WIN-UUTGUKP77SJ>
2016-05-19 14:29:35 +0800

    1

---
+++ node/server.js
@@ -0,0 +1,17 @@
+var cluster = require('cluster');
+var os= require('os');
+
+
+
+
+process.
+console.dir(os.totalmem(););
+console.dir(os.arch());
+console.dir(os.freemem(););
+console.dir(os.hostname());
+console.dir(os.cpus());
+console.dir(os.arch());
+console.dir(os.arch());
+console.dir(os.arch());
+console.dir(os.platform());
+console.dir(os.release());
